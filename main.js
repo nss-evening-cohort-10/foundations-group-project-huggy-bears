c4c22d6f2cc8d44a143e708c56507d13e0d56876
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-09-18 15:58:22 -0500

    added tech imgs cards show 2 per row

--- main.js
+++ main.js
@@ -2,30 +2,69 @@ const albums = [
 
     {
        albumName: 'Tech City',
-       image:'img/img_placeholder.png',
-       songs: ['Born In America', 'test song 2','test song 3']
+       image:'img/MCT.png',
+       songs: ["Nashville Yards", "Dev Heights","Who's Hiring"]
     },
     {
-      albumName: 'Boots Who',
-      image:'img/img_placeholder.png',
-      songs: ['test song1', 'test song 2','test song 3']  
+      albumName: 'Bootstrap',
+      image:'img/Bootstrap.png',
+      songs: ['Nav Bar', 'Buttons','Jumbotron']  
     },
     {
-      albumName: 'Safari Survives',
-      image:'img/img_placeholder.png',
-      songs: ['test song1', 'test song 2','test song 3']
+      albumName: 'Safari',
+      image:'img/safari.jpeg',
+      songs: ['Lonely', 'Google Who','Better than Yahoo']
     },
     {
-      albumName: 'Bezos Wins',
-       image:'img/img_placeholder.png',
-       songs: ['test song1', 'test song 2','test song 3']
+      albumName: 'Alphabet Inc.',
+       image:'img/Google.jpeg',
+       songs: ['Google It', 'We are the Champions','Never Gonna Quit']
+    },
+    {
+      albumName: 'Apple',
+      image:'img/apple.jpeg',
+      songs: ['Touch Screen', 'iPhone','Siri']
+    },
+    {
+      albumName: 'Bezos',
+       image:'img/download.png',
+       songs: ['Ruler of Tech', 'Pioneer','AWS']
     }];
 
 
+const printToDom = (stringToPrint, divId) =>{
+  document.getElementById(divId).innerHTML = stringToPrint;
+};
+
+
+const albumscardBuilder = () => {
+  let domString = '';
+  for(let i =0;i<albums.length;i++){
+    if(i % 2 === 0){
+      domString += `<div class="row">`
+    }
+    let album= albums[i];
+  domString +=`
+  <div class="album-cards text-center col-6">
+  <div class="card" >
+  <img src="${albums[i].image}" class="card-img-top image rounded" alt="...">
+    <div class="card-body ">
+      <h2 class="card-title">${albums[i].albumName}</h2>
+      <p class="card-text">${albums[i].songs.join('<br>')}</p>
+    </div>
+  </div> 
+  <button type="button" class="btn btn-primary play">Play</button>
+  <button type="button" class="btn btn-secondary purchase">Purchase</button>
+  </div>`;
+  if(i % 2 === 1){
+    domString += `</div>`;
+  }
+}
+  printToDom(domString,'albumsDiv');
+};
+albumscardBuilder();
 
   
-
-
 
 
 

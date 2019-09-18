b02e3eab50cef95c231b17752e3a2df8581491cb
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-09-17 19:03:41 -0500

    add data and img folder

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log('bite me');
+const albums = [
+
+    {
+       albumName: 'Tech City',
+       image:'img/img_placeholder.png',
+       songs: ['Born In America', 'test song 2','test song 3']
+    },
+    {
+      albumName: 'Boots Who',
+      image:'img/img_placeholder.png',
+      songs: ['test song1', 'test song 2','test song 3']  
+    },
+    {
+      albumName: 'Safari Survives',
+      image:'img/img_placeholder.png',
+      songs: ['test song1', 'test song 2','test song 3']
+    },
+    {
+      albumName: 'Bezos Wins',
+       image:'img/img_placeholder.png',
+       songs: ['test song1', 'test song 2','test song 3']
+    }];
+
+
+
+  
+
+
+
+
+
+
+
+
+

4491af7c0f673172e9ad070955275850ce2196e7
Gabriel Seals <sealsgj@gmail.com>
2019-09-18 19:00:39 -0500

    updating tour, js, and css

--- main.js
+++ main.js
@@ -1,35 +1,66 @@
-const albums = [
+const printToDom = (toPrint, divId) => {
+  document.getElementById(divId).innerHTML = toPrint
+}
 
-    {
-       albumName: 'Tech City',
-       image:'img/img_placeholder.png',
-       songs: ['Born In America', 'test song 2','test song 3']
-    },
-    {
-      albumName: 'Boots Who',
-      image:'img/img_placeholder.png',
-      songs: ['test song1', 'test song 2','test song 3']  
-    },
-    {
-      albumName: 'Safari Survives',
-      image:'img/img_placeholder.png',
-      songs: ['test song1', 'test song 2','test song 3']
-    },
-    {
-      albumName: 'Bezos Wins',
-       image:'img/img_placeholder.png',
-       songs: ['test song1', 'test song 2','test song 3']
-    }];
+const upcomingTours = [
+{
+cityImage: 'https://www.planetware.com/photos-large/USWI/us-wisconsin-milwaukee-lakefront.jpg',
+cityAndState:  'Milwaukee, WI',
+date: '11/20/3030',
+venue: 'The Rave/Eagles Club',
+tickets: 'https://www.therave.com/concert_details.asp?id=7067'
+},
+{
+cityImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-_VlbSxkLolq_-cvs1QIl3J4y0YpzHqJ4pMMUPj_TPtqtEQwBzg',
+cityAndState:  'Carbondale, IL',
+date: '01/02/3034',
+venue: 'The Hangar 9',
+tickets: 'http://thehangar9.ticketfly.com/'
 
+},
+{
+cityImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpwwW_2guk0ZkDMBrgqLarerFZnFVDJPfNyCbnELyYmQXgpOLnnQ',
+cityAndState: 'Tuscon, AZ',
+date: '05/04/3037',
+venue: 'The Rialto Theater',
+tickets: 'https:www.rialtotheatre.com/'
+},
+{
+cityImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgELPM5O8c91uOyWMq6-iz_OCtNfQgRJvORvO1yzsLbIaFchxm',
+cityAndState:  'Ypsilanti, MI',
+date: '06/12/3040',
+venue: 'EMU Convocation Center',
+tickets: 'https://www.emich.edu/emutix/'
+}
+]
 
+const tourLoopBuilder = (cardData) => {
+  let tourdom = ''
+  for (let i = 0; i < cardData.length; i++) {
+      tourdom += `
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-3">
+                <img src="${cardData[i].cityImage}" class="card-img" alt="...">
+              </div>
+              <div class="col-md-9">
+                <div class="card-body">
+                  <h5 class="card-title">${cardData[i].cityAndState}</h5>
+                  <p class="card-text">${cardData[i].date}</p>
+                  <p class="card-text d-inline">${cardData[i].venue}</p>
+                  <a href="${cardData[i].tickets}" role="button" class="btn btn-primary" id="tixFloat">Purchase Tickets</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+      `
+  }
+  printToDom(tourdom, 'cardArea')
+}
 
-  
-
-
-
-
-
-
+document.getElementById('tour').addEventListener('click', () => {
+tourLoopBuilder(upcomingTours);
+});
 
 
 
